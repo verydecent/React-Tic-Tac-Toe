dece041cd202d43a9b66ddd130d691f7ce68274a
verydecent <wnjhwng@gmail.com>
2018-09-29 22:07:51 -0700

    Worth

---
+++ src/Components/Square.js
@@ -0,0 +1,11 @@
+class Square extends React.Component {
+    render() {
+      return (
+        <button className="square">
+          {/* TODO */}
+        </button>
+      );
+    }
+  }
+  
+export default Square;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Game from './Game';
+
+const root = document.getElementById('root');
+
+ReactDOM.render(
+<Game />, 
+root);
+registerServiceWorker();
